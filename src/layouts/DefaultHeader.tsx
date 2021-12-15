3b59e72156edddbdcb0615607a6ffc15c0ec8690
durashere <21038648+durashere@users.noreply.github.com>
2021-12-15 10:54:40 +0100

    feat: add authentication with google

--- src/layouts/DefaultHeader.tsx
+++ src/layouts/DefaultHeader.tsx
@@ -1,4 +1,7 @@
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { MdMenu } from 'react-icons/md';
+import { ReactElement } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useRouter } from 'next/router';
 import {
   Button,
@@ -14,34 +17,54 @@ import {
   useDisclosure,
   VStack,
 } from '@chakra-ui/react';
-import { ReactElement } from 'react';
+
+import { auth } from '@/lib/firebase/clientApp';
 
 const menuItems = [{ name: 'Equipment', path: '/' }];
 
-const DefaultHeader = (): ReactElement => {
+const DefaultHeader = (): ReactElement | null => {
   const { pathname, push } = useRouter();
-
+  const [user] = useAuthState(auth);
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const activePage = menuItems.find((item) => item.path === pathname);
 
+  const signInWithGoogle = async (): Promise<void> => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider);
+  };
+
   return (
     <Flex as="header" align="center" gap={4} mb={4}>
       <IconButton aria-label="open drawer" icon={<MdMenu />} onClick={onOpen} />
-      <Heading as="h1" fontSize="3xl" fontWeight="bold" flexGrow={1}>
+      <Heading as="h1" fontSize="xl" fontWeight="semibold" flexGrow={1}>
         {activePage?.name}
       </Heading>
       <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
         <DrawerOverlay />
         <DrawerContent>
           <DrawerCloseButton />
-          <DrawerHeader>Navigation</DrawerHeader>
+          <DrawerHeader>
+            Hello, {user ? user.displayName : 'Stranger'}
+          </DrawerHeader>
           <DrawerBody>
-            <VStack as="nav">
+            {user ? (
+              <Button
+                colorScheme="blue"
+                isFullWidth
+                onClick={(): Promise<void> => signOut(auth)}
+              >
+                Logout
+              </Button>
+            ) : (
+              <Button colorScheme="blue" isFullWidth onClick={signInWithGoogle}>
+                Login with Google
+              </Button>
+            )}
+            <VStack mt={8} as="nav">
               {menuItems.map((item) => (
                 <Button
                   isFullWidth
-                  item={item}
                   key={item.name}
                   onClick={(): void => {
                     push(item.path);
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,17 +1,38 @@
+import { Box, Container, Heading } from '@chakra-ui/react';
 import { ReactElement } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
-import { Box, Container } from '@chakra-ui/react';
+import { auth } from '@/lib/firebase/clientApp';
 import DefaultHeader from '@/layouts/DefaultHeader';
 
 interface IDefaultLayout {
   children: React.ReactNode;
 }
 
-const DefaultLayout = ({ children }: IDefaultLayout): ReactElement => (
-  <Container py={4} minH="100vh">
-    <DefaultHeader />
-    <Box as="main">{children}</Box>
-  </Container>
-);
+const DefaultLayout = ({ children }: IDefaultLayout): ReactElement | null => {
+  const [user, loading] = useAuthState(auth);
+
+  return (
+    <Container py={4} minH="100vh">
+      <DefaultHeader />
+      <Box as="main">
+        {!loading && user && children}
+        {!loading && !user && (
+          <Heading
+            as="h2"
+            borderRadius="md"
+            borderWidth={1}
+            fontSize="xl"
+            fontWeight="semibold"
+            p={4}
+            textAlign="center"
+          >
+            Login inside sidebar to get access
+          </Heading>
+        )}
+      </Box>
+    </Container>
+  );
+};
 
 export default DefaultLayout;
